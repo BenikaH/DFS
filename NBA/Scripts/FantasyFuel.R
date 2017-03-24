06dd723996a390a9e7fcb2d6799b3c493a0d1004
aitken711 <aitken711@gmail.com>
2017-03-24 10:14:23 -0400

    Changed FantasyFuel logic
    Changed for loop to sapply to improve performance

--- NBA/Scripts/FantasyFuel.R
+++ NBA/Scripts/FantasyFuel.R
@@ -6,24 +6,21 @@ remDr <- rD$client
 remDr$navigate(paste0("http://www.dailyfantasyfuel.com/nba/projections/", tolower(site)))
 Sys.sleep(1)
 
-# initialize data frame
-pool <- data.frame("Player" = character(), "FantasyFuel" = character(), stringsAsFactors = F)
 # scrape projections
-for (i in 1:1000) {
-  webElem <- tryCatch({
-    remDr$findElement('xpath', paste0('//*[@id="projections"]/div[1]/div/div[5]/div[', i, ']'))
-  }, error = function(e) {
-    NULL
-  })
-  if (is.null(webElem)) {
-    break
-  }
-  pool[i, ] <- c(paste(webElem$getElementAttribute("data-first_name"),
+webElems <- remDr$findElements('xpath', paste0('//*[@id="projections"]/div[1]/div/div[5]/div'))
+getAttributes <- function(webElem) {
+  as.character(c(paste(webElem$getElementAttribute("data-first_name"),
                        webElem$getElementAttribute("data-last_name")),
-                 webElem$getElementAttribute("data-ppg_projected"))
+                 webElem$getElementAttribute("data-ppg_projected")))
 }
+
+# create player pool
+pool <- data.frame(t(sapply(webElems, getAttributes)), stringsAsFactors = F)
+# set column names
+names(pool) <- c("Player", "FantasyFuel")
 # convert projections from character to numeric
 pool$FantasyFuel <- as.numeric(pool$FantasyFuel)
+
 # close remote driver
 remDr$close()
 rD$server$stop()
